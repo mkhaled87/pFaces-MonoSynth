0cc53fdbf0ea3d3de214094c17fbfa2c3124fd4f
YasinSonmez <sonmezyasin6006@gmail.com>
2025-10-27 11:49:15 -0700

    monotone system acc example implemented without communication with pfaces

---
+++ examples/safe_set_monotone/run_safe.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+../../../../bin/pfaces -CG -k mono_synth.cpu@../../kernel-pack -cfg ./safe_set.cfg -d 1 -v 2 -p
+
---
+++ examples/safe_set_monotone/run_safe_3d.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+../../../../bin/pfaces -CG -k mono_synth.cpu@../../kernel-pack -cfg ./safe_set_3d.cfg -d 1 -v 2 -p
+
