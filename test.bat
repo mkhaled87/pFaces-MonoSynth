d600fd5335a5d99d8d4e00c031aaf89d6266cbfb
Yasin Sonmez <sonmezyasin6006@gmail.com>
2026-09-08 12:12:53 -0700

    Remove stale Windows scripts and superseded POC benchmark configs

--- test.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-
-rem Testing example: ACC
-cd examples\acc
-pfaces -CGH -k kernel.cpu@..\..\kernel-pack -cfg .\acc.cfg -d 1 -p
-cd ..\..
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Testing example: ACC
-cd examples/turn_ego_first
-pfaces -GH -k mono_synth.cpu@../../kernel-pack -cfg ./turn_ego_first.cfg -d 1 -p -v1
-cd ../..
-
